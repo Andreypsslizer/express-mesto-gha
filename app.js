331b0bd01f5451669d87963c396284c9a161614a
Andrew <akovzika@yandex.by>
2022-11-09 23:21:41 +0300

    pr14 first release

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ router.post('/signin', validateLogin, login);
 router.post('/signup', validateCreateUser, createUser);
 router.use('/users', auth, usersRouter);
 router.use('/cards', auth, cardsRouter);
+app.use(router)
 app.use('*', (req, res) => res.status(404).send({ message: '404 — Запрашиваемый ресурс не найден' }));
 
 app.listen(3000, () => {

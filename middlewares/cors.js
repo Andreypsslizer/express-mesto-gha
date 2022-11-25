3bbe3e4a0754b56073a1728fe31a7d921885ccac
Андрей Ковзик <akovzika@yandex.by>
2022-11-26 01:26:19 +0300

    pivo pit

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -7,24 +7,22 @@ const allowedCors = [
   'http://localhost:3000',
 ];
 
-const corsRequest = ((req, res, next) => {
+const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+
+const corsRequest = (req, res, next) => {
+  const { method } = req;
   const { origin } = req.headers;
+  const requestHeaders = req.headers['access-control-request-headers'];
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Credentials', true);
   }
-  const { method } = req;
-  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
-  }
-  const requestHeaders = req.headers['access-control-request-headers'];
-  if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Headers', requestHeaders);
     return res.end();
   }
 
   next();
-});
+};
 
 module.exports = corsRequest;
